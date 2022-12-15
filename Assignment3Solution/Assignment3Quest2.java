dca94122673ef77a904237551958a15764b9d3f5
Surya Pratap Singh <72143906+surya123-ctrl@users.noreply.github.com>
2022-12-15 15:04:49 +0530

    Assignment 3 question 2 done

---
+++ Assignment3Solution/Assignment3Quest2.java
@@ -0,0 +1,43 @@
+
+package Assignment3Solution;
+
+
+import java.util.Scanner;
+
+//You will be given an input number N and N numbers following it as the input. You are also given an integer
+//        K which represents how many times you have to rotate the array. Rotate the array
+public class Assignment3Quest2 {
+    static void rotateRight(int arr[], int k, int n) {
+        int temp[] = new int[n];
+        k %= n;
+        int p = 0;
+        for(int i=n-k; i<n; i++) temp[p++] = arr[i];
+        for(int i=0; i<n-k; i++) temp[p++] = arr[i];
+        for(int i=0; i<n; i++) System.out.print(temp[i] + " ");
+    }
+    static void rotateLeft(int arr[], int k, int n) {
+        int temp[] = new int[n];
+        k *= -1;
+        k %= n;
+        int p = 0;
+        for(int i=k; i<n; i++) temp[p++] = arr[i];
+        for(int i=0; i<k; i++) temp[p++] = arr[i];
+        for(int i=0; i<n; i++) System.out.print(temp[i] + " ");
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.print("Enter size of array : ");
+        n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0; i<n; i++) arr[i] = sc.nextInt();
+        int k = sc.nextInt();
+        if(k == 0) {for(int i=0; i<n; i++) System.out.print(arr[i] + " ");}
+        else if(k > 0) {
+            rotateRight(arr, k, n);
+        }
+        else {
+            rotateLeft(arr, k, n);
+        }
+    }
+}
