23f95b3b60e07ef9567baf107795f23970a43f80
Surya Pratap Singh <72143906+surya123-ctrl@users.noreply.github.com>
2022-12-30 21:34:21 +0530

    Learned Collections

--- ClassWork.java
+++ ClassWork.java
@@ -1,41 +1,90 @@
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.*;
 
 public class ClassWork {
-    public static void doMoreStuff() {
-        System.out.println(10/0);
-    }
-    public static void doStuff() {
-        doMoreStuff();
-    }
+
     public static void main(String[] args) {
-//        String str1 = new String("Hello");
-//        str1.concat("world");
-//        System.out.println(str1);
-//        StringBuffer str2 = new StringBuffer("Hello");
-//        str2.append("World");
-//        System.out.println(str2);
-//        String s1 = new String("Hello");
-//        String s2 = new String("Hello");
-//        System.out.println(s1 == s2);
-//        System.out.println(s1.equals(s2));
-//        StringBuffer s3 = new StringBuffer("Hello");
-//        StringBuffer s4 = new StringBuffer("Hello");
-//        System.out.println(s3 == s4);
-//        System.out.println(s3.equals(s4));
-//        final StringBuffer a1 = new StringBuffer("Hello");
-//        a1.append("World");
-//        System.out.println(a1);
-//        StringBuffer a2 = new StringBuffer("monday");
-//        System.out.println(a2);
-        try{
-            Scanner sc = new Scanner(System.in);
-            int n;
-            n = sc.nextInt();
-            System.out.println(n);
-        } catch (InputMismatchException e) {
-            System.out.println("Not inputted integer value");
-        }
-//        doStuff();
+//        ArrayList<String> students= new ArrayList<>();
+//        students.add("Surya");
+//        students.add("Arya");
+//        System.out.println(students);
+//        students.add(1,"Krishna");
+//        System.out.println(students);
+//        ArrayList<Integer> uid = new ArrayList<>();
+//        uid.add(12);
+//        uid.add(13);
+//        uid.add(14);
+//        System.out.println(uid);
+//        students.addAll(uid);
+//        System.out.println(students);
+//        uid.get(1);
+//        uid.remove(1);
+//        System.out.println(uid);
+//        uid.remove(Integer.valueOf(12));
+//        System.out.println(uid);
+//        students.set(1,"India");
+//        System.out.println(students);
+//        System.out.println(!students.contains("India"));
+//        for(int i=0; i<students.size(); i++) System.out.println(students.get(i));
+//        for (String it: students) {
+//            System.out.println(it);
+//        }
+//        Iterator<String> it = students.iterator();
+//        while (it.hasNext()) {
+//            System.out.println(it.next());
+//        }
+
+//        Stack<Integer> st = new Stack<>();
+//        st.push(1);
+//        st.push(2);
+//        st.push(3);
+//        st.push(4);
+//        System.out.println(st.peek());
+//        st.pop();
+//        System.out.println(st.peek());
+
+
+//        Queue<Integer> queue = new LinkedList<>();
+//        queue.offer(12);
+//        queue.offer(24);
+//        queue.offer(36);
+//        queue.offer(48);
+//        System.out.println(queue);
+//        System.out.println(queue.peek());
+//        queue.poll();
+//        System.out.println(queue);
+
+
+//        ArrayDeque<Integer> adq = new ArrayDeque<>();
+//        adq.offer(23);
+//        adq.offerFirst(43);
+//        adq.offerLast(56);
+//        System.out.println(adq);
+//        System.out.println(adq.peek());
+//        System.out.println(adq.peekFirst());
+//        System.out.println(adq.peekLast());
+
+//        Set<Integer> s = new HashSet<>();
+//        s.add(11);
+//        s.add(2);
+//        s.add(31);
+//        s.add(43);
+//        s.add(5);
+//        System.out.println(s);
+//        s.remove(31);
+//        System.out.println(s);
+//        System.out.println(s.contains(11));
+
+//        Set<Integer> s = new TreeSet<>();
+//        s.add(11);
+//        s.add(43);
+//        s.add(1);
+//        System.out.println(s);
+
+
+        HashMap<String, Integer> mp = new HashMap<>();
+        mp.put("abc", 1);
+        mp.put("def", 2);
+        mp.put("ghi", 3);
+        System.out.println(mp);
     }
 }
